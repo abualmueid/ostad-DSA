790ad45614b28882746f1efe8602f75e45df96ce
Abu Al Mueid <61595430+abualmueid@users.noreply.github.com>
2025-02-11 00:09:02 +0600

    Add file

---
+++ AssignmentOnArray/Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿//int[] nums = { 3, 6, 9, 1 };
+int[] nums = { 10 };
+
+Console.WriteLine(MaxGap(nums));
+
+int MaxGap(int[] nums)
+{
+    if (nums.Length < 2)
+    {
+        return 0;
+    }
+
+    Array.Sort(nums);
+
+    int maxGap = 0;
+    for (int i = 1; i < nums.Length; i++)
+    {
+        maxGap = Math.Max(maxGap, nums[i] - nums[i - 1]);
+    }
+
+    return maxGap;
+}
