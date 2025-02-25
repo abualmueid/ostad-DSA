e9e531eb2ef75ac3ab665f7cc65866ee53ea5bc9
Abu Al Mueid <61595430+abualmueid@users.noreply.github.com>
2025-02-25 19:00:30 +0600

    Add Task1 code

---
+++ AssignmentOnRecursion/Task1/Program.cs
@@ -0,0 +1,41 @@
+﻿#region Using Loop
+/*
+int n = int.Parse(Console.ReadLine());
+
+bool IsPowerOfFive(int n) 
+{
+    if (n < 1) return false;
+
+    for (int x = 0; Math.Pow(5, x) <= n; x++)
+    {
+        if (Math.Pow(5, x) == n)
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+Console.WriteLine(IsPowerOfFive(n));
+*/
+#endregion
+
+#region Using Recursion
+
+int n = int.Parse(Console.ReadLine());
+
+bool IsPowerOfFive(int n, int x = 0) 
+{
+    if (n < 1) return false;
+
+    int power = (int)Math.Pow(5, x);
+    if (power == n) return true;
+    if (power > n) return false;
+
+    return IsPowerOfFive(n, ++x);
+}
+
+Console.WriteLine(IsPowerOfFive(n));
+
+#endregion
