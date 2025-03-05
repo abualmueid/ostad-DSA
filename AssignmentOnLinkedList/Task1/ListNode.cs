405d924c56b25f237dca9b89f1a7c5688e425ab8
Abu Al Mueid <61595430+abualmueid@users.noreply.github.com>
2025-03-05 22:04:49 +0600

    Add file

---
+++ AssignmentOnLinkedList/Task1/ListNode.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class ListNode
+    {
+        public char value;
+        public ListNode next;
+        public ListNode prev;
+
+        public ListNode(char value)
+        {
+            this.value = value;
+            next = null;
+            prev = null;
+        }
+    }
+}
