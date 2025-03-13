16b94e690935176dfdbc817c6f2c8ac8d474f47b
Abu Al Mueid <61595430+abualmueid@users.noreply.github.com>
2025-03-13 22:48:49 +0600

    Add file

---
+++ AssignmentOnStackAndQueue/Task2/Program.cs
@@ -0,0 +1,64 @@
+﻿int q = int.Parse(Console.ReadLine());
+
+var stack1 = new Stack<int>();
+var stack2 = new Stack<int>();
+var queue = new Queue<int>();
+
+for (int i = 0; i < q; i++)
+{
+    string[] input = Console.ReadLine().Split();
+
+    if (input[0] == "1")
+    {
+        int x = int.Parse(input[1]);
+        stack1.Push(x);
+    }
+    else if (input[0] == "2")
+    {
+        MoveItemFromStack1ToStack2();
+        AddItemFromStack2ToQueue();
+        DequeueAndShowFront();
+    }
+    else if (input[0] == "3")
+    {
+        MoveItemFromStack1ToStack2();
+        AddItemFromStack2ToQueue();
+        ShowFront();
+    }
+}
+void MoveItemFromStack1ToStack2()
+{
+    if (stack2.Count == 0)
+    {
+        while (stack1.Count > 0)
+        {
+            stack2.Push(stack1.Pop());
+        }
+    }
+}
+
+void AddItemFromStack2ToQueue()
+{
+    while (stack2.Count > 0)
+    {
+        queue.Enqueue(stack2.Pop());
+    }
+}
+
+void DequeueAndShowFront() 
+{
+    queue.Dequeue();
+
+    if (queue.Count == 0)
+        Console.WriteLine("-1");
+    else
+        Console.WriteLine(queue.Peek());
+}
+
+void ShowFront()
+{
+    if (queue.Count == 0)
+        Console.WriteLine("-1");
+    else
+        Console.WriteLine(queue.Peek());
+}
