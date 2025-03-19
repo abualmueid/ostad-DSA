3c95448859d5aba6306b5350ba7bbf2b6545abd0
Abu Al Mueid <61595430+abualmueid@users.noreply.github.com>
2025-03-19 16:18:13 +0600

    Add file

---
+++ AssignmentOnSorting/Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿using Task1;
+
+int n = int.Parse(Console.ReadLine());
+var patients = new List<Patient>();
+
+for (int i = 0; i < n; i++)
+{
+    string[] input = Console.ReadLine().Split();
+
+    var patient = new Patient
+    {
+        Id = int.Parse(input[0]),
+        Name = input[1],
+        Age = int.Parse(input[2]),
+        Severity = int.Parse(input[3])
+    };
+
+    patients.Add(patient);
+}
+
+var sortedPatients = patients.OrderByDescending(p => p.Severity).ThenBy(p => p.Age).ToList();
+
+Console.WriteLine();
+foreach (var patient in sortedPatients)
+{
+    Console.WriteLine($"{patient.Id} {patient.Name} {patient.Age} {patient.Severity}");
+}
