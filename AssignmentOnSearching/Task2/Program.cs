c69d8c7ba477879af732b607a3d7bccdf0c5edd5
Abu Al Mueid <61595430+abualmueid@users.noreply.github.com>
2025-03-27 16:44:13 +0600

    Add file

---
+++ AssignmentOnSearching/Task2/Program.cs
@@ -0,0 +1,36 @@
+﻿string[] input = Console.ReadLine().Split();
+int n = int.Parse(input[0]);
+int k = int.Parse(input[1]);
+int[] a = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int[] query = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+for (int i = 0; i < k; i++)
+{
+    int index = FindMaxIndex(query[i]);
+    Console.WriteLine(index == -1 ? 0 : index + 1);
+}
+
+int FindMaxIndex(int x) 
+{
+    int l = 0;
+    int r = n - 1;
+    int result = -1;
+
+    while (l <= r)
+    {
+        int mid = (l + r) / 2;
+
+        if (x >= a[mid])
+        {
+            result = mid;
+            l = mid + 1;
+        }
+        else
+        {
+            r = mid - 1;
+        }
+    }
+
+    return result;
+}
+
