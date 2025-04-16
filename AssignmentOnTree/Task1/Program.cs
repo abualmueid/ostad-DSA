28b6edc70c70fd5e1ec6ddb24cec60714b87674f
Abu Al Mueid <61595430+abualmueid@users.noreply.github.com>
2025-04-16 17:07:39 +0600

    Add file

---
+++ AssignmentOnTree/Task1/Program.cs
@@ -0,0 +1,53 @@
+﻿using Task1;
+
+var nodes = new List<TreeNode>();
+var InOrderOutput = new List<int>();
+var PreOrderOutput = new List<int>();
+var PostOrderOutput = new List<int>();
+
+int n = int.Parse(Console.ReadLine());
+for (int i = 0; i < n; i++)
+{
+    string[] input = Console.ReadLine().Split();
+    int val = int.Parse(input[0]);
+    int left = int.Parse(input[1]);
+    int right = int.Parse(input[2]);
+
+    nodes.Add(new TreeNode(val, left, right));
+}
+
+InOrder(0);
+PreOrder(0);
+PostOrder(0);
+
+Console.WriteLine();
+Console.WriteLine(string.Join(" ", InOrderOutput));
+Console.WriteLine(string.Join(" ", PreOrderOutput));
+Console.WriteLine(string.Join(" ", PostOrderOutput));
+
+void InOrder(int index)
+{
+    if (index == -1) return;
+
+    InOrder(nodes[index].left);
+    InOrderOutput.Add(nodes[index].val);
+    InOrder(nodes[index].right);
+}
+
+void PreOrder(int index)
+{
+    if (index == -1) return;
+
+    PreOrderOutput.Add(nodes[index].val);
+    PreOrder(nodes[index].left);
+    PreOrder(nodes[index].right);
+}
+
+void PostOrder(int index)
+{
+    if (index == -1) return;
+
+    PostOrder(nodes[index].left);
+    PostOrder(nodes[index].right);
+    PostOrderOutput.Add(nodes[index].val);
+}
---
+++ AssignmentOnTree/Task1/TreeNode.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class TreeNode
+    {
+        public int val;
+        public int left;
+        public int right;
+
+        public TreeNode(int val, int left, int right)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
